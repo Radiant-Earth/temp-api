24fd83f552b5c43040bb2d6523f20b350a757b16
jothamardel <ardelmbiplang@gmail.com>
2022-12-22 23:41:11 +0100

    chore: implementing views

--- src/controller/market.controller.js
+++ src/controller/market.controller.js
@@ -1,30 +1,28 @@
 const { Market } = require("../model/market.model");
 
 async function createMarket(req, res) {
-  
   try {
-
     for (let index = 0; index < req.body.length; index++) {
-      
-      let data = await Market.updateOne({ 
-        metadata_id: req.body[index]["metadata_id"] }, 
-        { ...req.body[index] }, {upsert: true});
+      let data = await Market.updateOne(
+        {
+          metadata_id: req.body[index]["metadata_id"],
+        },
+        { ...req.body[index] },
+        { upsert: true }
+      );
 
       console.log("Does List NFT exist? ", data);
-      
+
       if (!data) {
         const listing = new Market({
           ...req.body[index],
         });
-    
+
         await listing.save();
       }
-      
     }
 
-
     getAllListedCoupons(req, res);
-
   } catch (error) {
     console.log(error);
     res.status(400).json({
@@ -36,113 +34,112 @@ async function createMarket(req, res) {
 async function getMyCoupons(req, res) {
   const id = req.params.id;
 
-  if(!id) return res.status(404).json({
-    message: 'something went wrong!'
-  })
+  if (!id)
+    return res.status(404).json({
+      message: "something went wrong!",
+    });
 
   try {
     const myCoupons = await Market.find({ account_id: id });
 
-    if (!myCoupons.length) return res.status(404).json({
-        message: 'No coupon found, create a coupon.'
-    })
-
+    if (!myCoupons.length)
+      return res.status(404).json({
+        message: "No coupon found, create a coupon.",
+      });
 
     return res.status(200).json({
-        message: 'successful!',
-        data: myCoupons
-    })
-
+      message: "successful!",
+      data: myCoupons,
+    });
   } catch (error) {
     res.status(400).json({
-        message: 'Something went wrong!'
-    })
+      message: "Something went wrong!",
+    });
   }
-
 }
 
-
-async function updateMyCoupon(req, res) {
+async function updateListedCoupon(req, res) {
   const id = req.params.id;
 
-  if(!id) return res.status(404).json({
-    message: 'something went wrong!'
-  })
+  if (!id)
+    return res.status(404).json({
+      message: "something went wrong! No ID.",
+    });
 
-  console.log(req.body)
+  console.log(req.body);
 
   try {
-    const coupon = await Market.findByIdAndUpdate({ _id: id}, { data: req.body.data, is_minted: req.body.is_minted}, { upsert: false});
+    const coupon = await Market.findByIdAndUpdate(
+      { _id: id },
+      { ...req.body },
+      { upsert: false }
+    );
 
-    console.log('Updated==========> ', coupon)
+    getAllListedCoupons(req, res);
+    // const coupon = await Market.findByIdAndUpdate({ _id: id}, { data: req.body.data, is_minted: req.body.is_minted}, { upsert: false});
 
-    const myCoupons = await Market.find({ account_id: req.body.data.ownerId});
+    // console.log("Updated==========> ", coupon);
 
-    if (!myCoupons.length) return res.status(404).json({
-        message: 'No coupon found, create a coupon.'
-    })
+    // const myCoupons = await Market.find({ account_id: req.body.data.ownerId });
 
+    // if (!myCoupons.length)
+    //   return res.status(404).json({
+    //     message: "No coupon found, create a coupon.",
+    //   });
 
-    return res.status(200).json({
-        message: 'successful!',
-        data: myCoupons
-    })
-
+    // return res.status(200).json({
+    //   message: "successful!",
+    //   data: myCoupons,
+    // });
   } catch (error) {
     res.status(400).json({
-        message: 'Something went wrong!'
-    })
+      message: "Something went wrong!",
+    });
   }
-
 }
 
 async function getAllListedCoupons(req, res) {
-
-
   try {
     const listedCoupons = await Market.find();
     return res.status(200).json({
-        message: 'successful!',
-        data: listedCoupons
-    })
-
+      message: "successful!",
+      data: listedCoupons,
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(400).json({
-        message: 'Something went wrong!'
-    })
+      message: "Something went wrong!",
+    });
   }
-
 }
-
 
 async function deleteCoupon(req, res) {
-  const id = req.params.id
+  const id = req.params.id;
 
   try {
-    const coupon = await Market.findOneAndDelete({ 
-      metadata_id: id })
-    console.log('Deleted coupon ========> ', coupon);
+    const coupon = await Market.findOneAndDelete({
+      metadata_id: id,
+    });
+    console.log("Deleted coupon ========> ", coupon);
     if (!coupon) {
       return res.status(404).json({
-        message: 'coupon doesn\'t exist!'
-      })
+        message: "coupon doesn't exist!",
+      });
     }
     return res.status(200).json({
-        message: 'successful!',
-    })
-
+      message: "successful!",
+    });
   } catch (error) {
     res.status(400).json({
-        message: 'Something went wrong!'
-    })
+      message: "Something went wrong!",
+    });
   }
-
 }
 
-
-
-
-
-
-module.exports = { createMarket, getMyCoupons, getAllListedCoupons, updateMyCoupon, deleteCoupon };
+module.exports = {
+  createMarket,
+  getMyCoupons,
+  getAllListedCoupons,
+  updateListedCoupon,
+  deleteCoupon,
+};
--- src/routes/market.routes.js
+++ src/routes/market.routes.js
@@ -1,16 +1,17 @@
 const express = require("express");
 
-const { 
-  createMarket, 
+const {
+  createMarket,
   getAllListedCoupons,
-  deleteCoupon
+  deleteCoupon,
+  updateListedCoupon,
 } = require("../controller/market.controller");
 
 const Routes = express.Router();
 
+Routes.post("/market/create", createMarket);
+Routes.post("/market/update/:id", updateListedCoupon);
+Routes.get("/market", getAllListedCoupons);
+Routes.delete("/market/:id", deleteCoupon);
 
-Routes.post('/market/create', createMarket);
-Routes.get('/market', getAllListedCoupons);
-Routes.delete('/market/:id', deleteCoupon);
-
-module.exports = Routes
+module.exports = Routes;

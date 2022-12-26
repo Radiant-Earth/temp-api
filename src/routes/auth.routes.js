a4bcfc6c182331f3b4d87ac90a12ed5b3829611f
jothamardel <ardelmbiplang@gmail.com>
2022-12-26 02:15:37 +0100

    chore: enabling users to update biodata

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
-const { httpCreateNewUser, httpLoginUser } = require("../controller/auth.controller");
+const {
+  httpCreateNewUser,
+  httpLoginUser,
+} = require("../controller/auth.controller");
 
 const Routes = express.Router();
 
+Routes.post("/signup", httpCreateNewUser);
+Routes.post("/signin", httpLoginUser);
+Routes.post("/update-user", httpLoginUser);
 
-Routes.post('/signup', httpCreateNewUser);
-Routes.post('/signin', httpLoginUser);
-
-module.exports = Routes
+module.exports = Routes;

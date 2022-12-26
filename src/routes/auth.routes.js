94bff05aa1c8cc78c03156e75f581df8d0675112
jothamardel <ardelmbiplang@gmail.com>
2022-12-26 02:27:43 +0100

    chore: updating user's route to use the correct controller

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,12 +3,13 @@ const express = require("express");
 const {
   httpCreateNewUser,
   httpLoginUser,
+  httpUpdateUser,
 } = require("../controller/auth.controller");
 
 const Routes = express.Router();
 
 Routes.post("/signup", httpCreateNewUser);
 Routes.post("/signin", httpLoginUser);
-Routes.post("/update-user", httpLoginUser);
+Routes.post("/update-user", httpUpdateUser);
 
 module.exports = Routes;

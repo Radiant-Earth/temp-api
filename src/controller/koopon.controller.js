328f610604f7804c5f2debbfcfb622b56314af23
jothamardel <ardelmbiplang@gmail.com>
2023-01-05 12:11:01 +0100

    chore: updating coupon controller to get coupons by country

--- src/controller/koopon.controller.js
+++ src/controller/koopon.controller.js
@@ -13,14 +13,14 @@ async function createKoopon(req, res) {
     quantity,
     store,
     title,
-    _id
+    _id,
   } = req.body;
   // if (!_id) return res.status(400).json({
   //   message: '_id cannot be blank!'
   // })
   try {
     let data = await Koopon.findOneAndUpdate({ _id }, { ...req.body });
-      
+
     const allData = await Koopon.find({ _id });
     console.log(data);
     if (data) {
@@ -69,111 +69,134 @@ async function createKoopon(req, res) {
 async function getMyCoupons(req, res) {
   const id = req.params.id;
 
-  if(!id) return res.status(404).json({
-    message: 'something went wrong!'
-  })
+  if (!id)
+    return res.status(404).json({
+      message: "something went wrong!",
+    });
 
   try {
     const myCoupons = await Koopon.find({ account_id: id });
 
-    if (!myCoupons.length) return res.status(404).json({
-        message: 'No coupon found, create a coupon.'
-    })
-
+    if (!myCoupons.length)
+      return res.status(404).json({
+        message: "No coupon found, create a coupon.",
+      });
 
     return res.status(200).json({
-        message: 'successful!',
-        data: myCoupons
-    })
-
+      message: "successful!",
+      data: myCoupons,
+    });
   } catch (error) {
     res.status(400).json({
-        message: 'Something went wrong!'
-    })
+      message: "Something went wrong!",
+    });
   }
-
 }
 
+async function getCouponsByCountry(req, res) {
+  const id = req.params.id;
+
+  if (!id)
+    return res.status(404).json({
+      message: "something went wrong!",
+    });
+
+  try {
+    const myCoupons = await Koopon.find({ country: id });
+
+    if (!myCoupons.length)
+      return res.status(404).json({
+        message: "No coupon found, create a coupon.",
+      });
+
+    return res.status(200).json({
+      message: "successful!",
+      data: myCoupons,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Something went wrong!",
+    });
+  }
+}
 
 async function updateMyCoupon(req, res) {
   const id = req.params.id;
 
-  if(!id) return res.status(404).json({
-    message: 'something went wrong!'
-  })
+  if (!id)
+    return res.status(404).json({
+      message: "something went wrong!",
+    });
 
-  console.log(req.body)
+  console.log(req.body);
 
   try {
-    const coupon = await Koopon.findByIdAndUpdate({ _id: id}, { data: req.body.data, is_minted: req.body.is_minted}, { upsert: false});
+    const coupon = await Koopon.findByIdAndUpdate(
+      { _id: id },
+      { data: req.body.data, is_minted: req.body.is_minted },
+      { upsert: false }
+    );
 
-    console.log('Updated==========> ', coupon)
+    console.log("Updated==========> ", coupon);
 
-    const myCoupons = await Koopon.find({ account_id: req.body.data.ownerId});
+    const myCoupons = await Koopon.find({ account_id: req.body.data.ownerId });
 
-    if (!myCoupons.length) return res.status(404).json({
-        message: 'No coupon found, create a coupon.'
-    })
-
+    if (!myCoupons.length)
+      return res.status(404).json({
+        message: "No coupon found, create a coupon.",
+      });
 
     return res.status(200).json({
-        message: 'successful!',
-        data: myCoupons
-    })
-
+      message: "successful!",
+      data: myCoupons,
+    });
   } catch (error) {
     res.status(400).json({
-        message: 'Something went wrong!'
-    })
+      message: "Something went wrong!",
+    });
   }
-
 }
 
 async function getAllCoupons(req, res) {
-
-
   try {
     const coupons = await Koopon.find();
     return res.status(200).json({
-        message: 'successful!',
-        data: coupons
-    })
-
+      message: "successful!",
+      data: coupons,
+    });
   } catch (error) {
     res.status(400).json({
-        message: 'Something went wrong!'
-    })
+      message: "Something went wrong!",
+    });
   }
-
 }
-
 
 async function deleteCoupon(req, res) {
-  const id = req.params.id
+  const id = req.params.id;
 
   try {
-    const coupon = await Koopon.findOneAndDelete({ _id: id })
-    console.log('Deleted coupon ========> ', coupon);
+    const coupon = await Koopon.findOneAndDelete({ _id: id });
+    console.log("Deleted coupon ========> ", coupon);
     if (!coupon) {
       return res.status(404).json({
-        message: 'coupon doesn\'t exist!'
-      })
+        message: "coupon doesn't exist!",
+      });
     }
     return res.status(200).json({
-        message: 'successful!',
-    })
-
+      message: "successful!",
+    });
   } catch (error) {
     res.status(400).json({
-        message: 'Something went wrong!'
-    })
+      message: "Something went wrong!",
+    });
   }
-
 }
 
-
-
-
-
-
-module.exports = { createKoopon, getMyCoupons, getAllCoupons, updateMyCoupon, deleteCoupon };
+module.exports = {
+  createKoopon,
+  getMyCoupons,
+  getAllCoupons,
+  updateMyCoupon,
+  deleteCoupon,
+  getCouponsByCountry,
+};
--- src/routes/koopon.routes.js
+++ src/routes/koopon.routes.js
@@ -1,15 +1,21 @@
 const express = require("express");
 
-const { createKoopon, getMyCoupons, getAllCoupons, updateMyCoupon, deleteCoupon } = require("../controller/koopon.controller");
+const {
+  createKoopon,
+  getMyCoupons,
+  getAllCoupons,
+  updateMyCoupon,
+  deleteCoupon,
+  getCouponsByCountry,
+} = require("../controller/koopon.controller");
 
 const Routes = express.Router();
-
 
 Routes.post("/koopon", createKoopon);
 Routes.get("/koopon", getAllCoupons);
 Routes.get("/koopon/:id", getMyCoupons);
+Routes.get("/koopon/country/:id", getCouponsByCountry);
 Routes.put("/koopon/:id", updateMyCoupon);
 Routes.delete("/koopon/:id", deleteCoupon);
-
 
 module.exports = Routes;

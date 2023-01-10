baa152ec51161995c3a741de2e23dd2210de3566
jothamardel <ardelmbiplang@gmail.com>
2023-01-10 13:53:39 +0100

    chore: changing nodemailer configurations

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -18,15 +18,27 @@ async function httpSendEmail(data) {
     });
   };
 
+  // let transporter = nodemailer.createTransport({
+  //   service: "gmail",
+  //   auth: {
+  //     type: "OAuth2",
+  //     user: process.env.MAIL_USERNAME,
+  //     pass: process.env.MAIL_PASSWORD,
+  //     clientId: process.env.OAUTH_CLIENTID,
+  //     clientSecret: process.env.OAUTH_CLIENT_SECRET,
+  //     refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+  //   },
+  // });
   let transporter = nodemailer.createTransport({
-    service: "gmail",
+    port: 465,
+    host: "smtp.zoho.com",
     auth: {
-      type: "OAuth2",
-      user: process.env.MAIL_USERNAME,
-      pass: process.env.MAIL_PASSWORD,
-      clientId: process.env.OAUTH_CLIENTID,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET,
-      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+      user: `${process.env.MAIL_NAME}`,
+      pass: `${process.env.MAIL_KEY}`,
+    },
+    secure: true,
+    tls: {
+      rejectUnauthorized: false,
     },
   });
 
